7bab3c3fe167867a1a5e5d0286ef0bc69cc814b8
Lakshya Goyal <lakshyagoyal34@gmail.com>
2025-09-07 21:20:10 +0530

    Integrated Frontend & Backend via Path

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -12,9 +14,10 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/get", (req, res)=>{
-  res.send("server is running")
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname,"../Frontend/build")));
   
 app.post("/api/gen", async(req, res)=>{
   try{
@@ -30,6 +33,12 @@ app.post("/api/gen", async(req, res)=>{
   
 })
 
-app.listen(4000, ()=>{
-  console.log("Server is Listening on 4000");
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname, "../Frontend/build", "index.html"));
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+  console.log(`Server is Listening on ${PORT}`);
 })
